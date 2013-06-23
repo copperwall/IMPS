a92e0ef5ddc58ebeb69d1be156eca3cc988cf8e7
omegaZero <copperwa@calpoly.edu>
2013-06-23 10:29:26 -0700

    more javadocs

--- Imps.java
+++ Imps.java
@@ -16,7 +16,7 @@ public class Imps {
       String input;
       File script;
 
-      // Checks to see if there is a script to read
+      /* Checks to see if there is a script to read */
       if (args.length == 2) {
          script = new File(args[1]);
          scan = new Scanner(script);
